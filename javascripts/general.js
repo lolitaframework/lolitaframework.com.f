b4670d04e82f6b853a3ce69a09babc501f33cdbf
vitalik1921 <vitalik1921@gmail.com>
2016-08-04 16:20:45 +0300

    Making HTML

--- javascripts/general.js
+++ javascripts/general.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function($) {
     $bottom_line = $('<span class="bottom_line"></span>');
     $menu_container.append($bottom_line);
 
-    $menu_container.children('li').hover(function() {
+    $menu_container.children('li.has-sub-menu').hover(function() {
     	$bottom_line.css('width', $items_width + 'px');
     }, function() {
     	$bottom_line.css('width', '0');
@@ -18,18 +18,21 @@ jQuery(document).ready(function($) {
 
     //Search button
     $search_panel = $('.search_panel');
-    $main_menu = $('.main_menu');
+    $front_block = $('.front_block');
+    $main_menu = $front_block.find('.lf_interface_menu');
     $('.lf_interface_menu.__hover_container li.search > a').click(function() {
-        $search_panel.toggleClass('opened');
-        $main_menu.toggleClass('closed');
+        $main_menu.addClass('menuAnimation').delay(200).queue(function(next) {
+            $search_panel.toggleClass('opened');
+            $front_block.toggleClass('closed');
+            $(this).removeClass('menuAnimation');
+            next();
+        })
     });
     $('.search_panel__close').click(function(e) {
         e.preventDefault();
         $search_panel.toggleClass('opened');
-        $main_menu.toggleClass('closed');        
+        $front_block.toggleClass('closed');        
     });
-
-
 });
 
 jQuery(window).load(function() {
