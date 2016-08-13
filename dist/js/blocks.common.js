dafaa32489b6f26085c49b36dae1eaaa63ad9e15
vitalik1921 <vitalik1921@gmail.com>
2016-08-13 15:07:24 +0300

    Added animations

---
+++ dist/js/blocks.common.js
@@ -0,0 +1,1 @@
+$l_front_block=$(".l-front-block"),$("body").on("w_menu__link__search_click",function(){$l_front_block.addClass("__hidden")}),$("body").on("w_search_block__close",function(){$l_front_block.removeClass("__hidden"),$l_front_block.addClass("__visible")});
