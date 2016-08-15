f463d866aaff98276082f072cbaa2aac853748bf
vitalik1921 <vitalik1921@gmail.com>
2016-08-15 11:28:30 +0300

    Making html

--- app/blocks/common/w-menu/js/w-menu.js
+++ app/blocks/common/w-menu/js/w-menu.js
@@ -1,3 +1,4 @@
+$w_menu = $('.w-menu');
 $w_menu__item = $('.w-menu__item');
 $w_menu__item__has_submenu = $('.w-menu__item.__has_submenu');
 $w_menu__container = $('.w-menu__container');
@@ -13,7 +14,6 @@ $w_menu__item.not(':last-child').each(
 
 // add line into menu container
 $w_menu__container.append($w_menu__undered_line);
-
 $w_menu__item__has_submenu.hover( 
 	function() {
 		$w_menu__undered_line.animate({'width': $w_menu_items_width + 'px'}, 100);
@@ -24,11 +24,28 @@ $w_menu__item__has_submenu.hover(
 );
 
 // add search button trigger
-
 $w_menu__link__search = $('.w-menu__link.__search');
-
 $w_menu__link__search.on('click', 
 	function() {
 		$('body').trigger('w_menu__link__search_click')
+	}
+);
+
+// add mobile elements
+$w_menu__mb_search = $('<button class="w-menu__mb-search">Search</button>');
+$w_menu__mb_menu_button = $('<button class="w-menu__mb-menu-button">Menu</button>');
+$w_menu.append($w_menu__mb_search);
+$w_menu.append($w_menu__mb_menu_button);
+
+$w_menu__mb_search.on('click', 
+	function() {
+		$('body').trigger('w_menu__mb_search_click');
+	}
+);
+
+$w_menu__mb_menu_button.on('click', 
+	function() {
+		$('body').trigger('w_menu__mb_menu_button_click');
+		$w_menu__container.slideToggle('400');
 	}
 );
