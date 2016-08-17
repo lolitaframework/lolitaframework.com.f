85a7c50dc63d67c58c4895a591589ffdc49bc980
vitalik1921 <vitalik1921@gmail.com>
2016-08-17 17:38:59 +0300

    Added 404

--- app/blocks/common/l-full-screen-block/js/l-full-screen-block.js
+++ app/blocks/common/l-full-screen-block/js/l-full-screen-block.js
@@ -15,14 +15,14 @@ $('body').on(
 );
 
 $('body').on(
-	'b-attachment__link_hovered',
+	'b-attachment__link_hovered, b-404__link_hovered',
 	function() {
 		$l_full_screen_block.addClass('__hovered_logo');	
 	}
 );
 
 $('body').on(
-	'b-attachment__link_unhovered',
+	'b-attachment__link_unhovered, b-404__link_unhovered',
 	function() {
 		$l_full_screen_block.removeClass('__hovered_logo');	
 	}
