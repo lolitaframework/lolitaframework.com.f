a3537ffedfcf51af91fb9e94ee90bb87b346e5e6
vitalik1921 <vitalik1921@gmail.com>
2016-08-20 16:27:10 +0300

    Refactored

--- app/blocks/common/l-full-screen-block/js/l-full-screen-block.js
+++ app/blocks/common/l-full-screen-block/js/l-full-screen-block.js
@@ -12,18 +12,4 @@ $('body').on(
 	function() {
 		$l_full_screen_block.removeClass('__with_logo');	
 	}
-);
-
-$('body').on(
-	'b-attachment__link_hovered, b-404__link_hovered',
-	function() {
-		$l_full_screen_block.addClass('__hovered_logo');	
-	}
-);
-
-$('body').on(
-	'b-attachment__link_unhovered, b-404__link_unhovered',
-	function() {
-		$l_full_screen_block.removeClass('__hovered_logo');	
-	}
-);
+);	
