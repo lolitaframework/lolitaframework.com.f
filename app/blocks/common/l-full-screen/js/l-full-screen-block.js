994e284f87d7f9504b8296641895b1cb16e5f8c4
vitalik1921 <vitalik1921@gmail.com>
2016-08-31 16:43:25 +0300

    Changed logo, added l-full-screen

---
+++ app/blocks/common/l-full-screen/js/l-full-screen-block.js
@@ -0,0 +1,15 @@
+$l_full_screen_block = $('.l-full-screen');
+
+$('body').on(
+	'w_menu__link__search_click',
+	function() {
+		$l_full_screen_block.addClass('__with_logo');
+	}
+);
+
+$('body').on(
+	'w_search_block__close',
+	function() {
+		$l_full_screen_block.removeClass('__with_logo');	
+	}
+);	
