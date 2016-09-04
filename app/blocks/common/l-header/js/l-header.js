de31112b76d8032a54ec4f2612266271bd65e3a8
vitalik1921 <vitalik1921@gmail.com>
2016-09-04 14:06:20 +0300

    Added search dropdown

--- app/blocks/common/l-header/js/l-header.js
+++ app/blocks/common/l-header/js/l-header.js
@@ -1,18 +1,18 @@
-$l_header__top__container__logo = $('.l-header__top__container__logo');
-$l_header__top__container__menu = $('.l-header__top__container__menu');
+$l_header__top__container__logo = $('.l-header__w-logo');
+$l_header__top__container__menu = $('.l-header__w-menu');
 
 $('body').on(
 	'w_menu__link__search_click', 
 	function() {
-		$l_header__top__container__logo.addClass('l-header__top__container__logo--hidden');
-		$l_header__top__container__menu.addClass('l-header__top__container__menu--hidden');
+		$l_header__top__container__logo.addClass('l-header__w-logo--hidden');
+		$l_header__top__container__menu.addClass('l-header__w-menu--hidden');
 	}
 );
 
 $('body').on(
 	'w_search_block__close', 
 	function() {
-		$l_header__top__container__logo.removeClass('l-header__top__container__logo--hidden');
-		$l_header__top__container__menu.removeClass('l-header__top__container__menu--hidden');
+		$l_header__top__container__logo.removeClass('l-header__w-logo--hidden');
+		$l_header__top__container__menu.removeClass('l-header__w-menu--hidden');
 	}
 );
