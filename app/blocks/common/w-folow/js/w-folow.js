ccbdfbdcf636da7accf2ebf4feaca8559783f1a2
vitalik1921 <vitalik1921@gmail.com>
2016-09-13 21:45:24 +0300

    Making html

--- app/blocks/common/w-folow/js/w-folow.js
+++ app/blocks/common/w-folow/js/w-folow.js
@@ -1,16 +1,20 @@
 $w_folow = $('.w-folow');
 $w_folow_frame = $w_folow.find('.w-folow__frame');
 
-$w_folow_frame.sly({
-    horizontal: 1,
-    itemNav: 'centered',
-    smart: 1,
-    activateMiddle: 1,
-    mouseDragging: 1,
-    touchDragging: 1,
-    startAt: 5,
-    scrollBy: 1,
-    speed: 100,
-    dragHandle: 1,
-    dynamicHandle: 1
-});
+
+if (jQuery(window).width() > 768) {
+    $w_folow_frame.sly({
+        horizontal: 1,
+        itemNav: 'centered',
+        smart: 1,
+        activateMiddle: 1,
+        mouseDragging: 1,
+        touchDragging: 1,
+        startAt: 5,
+        scrollBy: 1,
+        speed: 100,
+        dragHandle: 1,
+        dynamicHandle: 1
+    });
+
+}
