01939246ea4b8736d51bb200514b2cdcdb38290e
vitalik1921 <vitalik1921@gmail.com>
2016-09-14 17:21:23 +0300

    Search page

--- app/blocks/common/b-search-form/js/b-search-form.js
+++ app/blocks/common/b-search-form/js/b-search-form.js
@@ -1,7 +1,17 @@
-$b_search_form__input = $('.b-search-form__input:first');
+$b_search_form__input = $('.b-search-form--header .b-search-form__input, .b-search-form--header .b-search-form__input');
+$b_search_form_archive__input = $('.b-search-form--archive .b-search-form__input');
+$b_search_form__clear = $('.b-search-form__clear');
 
 $('body').on('w_menu__link__search_click', 
 	function() {
 		$b_search_form__input.focus();
 	}
 );
+
+$b_search_form_archive__input.focus();
+$b_search_form__clear.on('click', function(e) {
+	e.preventDefault();
+	$b_search_form_archive__input.val("");
+});
+
+
