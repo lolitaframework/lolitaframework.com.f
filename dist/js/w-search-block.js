7d4a5a597d505a339ce2275d50a65e8260dbd7ee
Vitaliy Shebela <vitalik.privat@gmail.com>
2016-11-20 00:15:34 +0200

    Search results keys navigation

--- dist/js/w-search-block.js
+++ dist/js/w-search-block.js
@@ -1,4 +1,5 @@
 var $w_search_block = jQuery('.w-search-block');
+var $w_search_block__items = $w_search_block.find('.w-search-block__item');
 var $w_search_block__close = jQuery('.w-search-block__close');
 
 // add overlay
@@ -14,17 +15,78 @@ $overlay.css({
 });
 jQuery('body').prepend($overlay);
 
-
+// search click
 jQuery('body').on('w_menu__link__search_click',
     function() {
         $w_search_block.removeClass('w-search-block--hidden');
         $w_search_block.addClass('w-search-block--visible');
+        $w_search_block.addClass('w-search-block--active');
         if ($w_search_block.hasClass('w-search-block--header')) {
             $overlay.show();
         }
     }
 );
 
+// w-search-block activation/deactivation
+jQuery(document).mouseup(function(e) {
+    if (!$w_search_block.is(e.target) // if the target of the click isn't the container...
+        && $w_search_block.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $w_search_block.removeClass('w-search-block--active');
+    } else {
+        $w_search_block.addClass('w-search-block--active');
+    }
+});
+
+// set next item
+function nextItem() {
+    var $next_element = $w_search_block__items.filter('.w-search-block__item--active').next();
+    $w_search_block__items.removeClass('w-search-block__item--active');
+    if (!$next_element.length) {
+        $next_element = $w_search_block__items.first();
+        console.log($next_element);
+    }
+    $next_element.addClass('w-search-block__item--active');
+}
+
+// set prev item
+function prevItem() {
+    var $prev_element = $w_search_block__items.filter('.w-search-block__item--active').prev();
+    $w_search_block__items.removeClass('w-search-block__item--active');
+    if (!$prev_element.length) {
+        $prev_element = $w_search_block__items.last();
+    }
+    $prev_element.addClass('w-search-block__item--active');
+}
+
+// bind hover
+$w_search_block__items.hover(function() {
+    $w_search_block__items.removeClass('w-search-block__item--active');
+    $(this).addClass('w-search-block__item--active');
+}, function() {
+    $w_search_block__items.removeClass('w-search-block__item--active');
+});
+
+// arrows binding
+jQuery(document).keydown(function(e) {
+    if ($w_search_block.hasClass('w-search-block--active')) {
+        switch (e.which) {
+            case 38: // up
+                prevItem();
+                break;
+
+            case 40: // down
+                nextItem();
+                break;
+
+            default:
+                return;
+        }
+        e.preventDefault();
+    }
+});
+
+// close button
 $w_search_block__close.on('click',
     function() {
         $w_search_block.addClass('w-search-block--hidden');
