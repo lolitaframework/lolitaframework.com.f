188fd7056daf8a3dcd3de9fbeb825fc0b6a79955
Vitaliy Shebela <vitalik@Vitaliys-MacBook-Pro.local>
2016-11-19 22:06:30 +0200

    Showcase - mobile version

--- app/blocks/common/b-stories/js/b-stories.js
+++ app/blocks/common/b-stories/js/b-stories.js
@@ -11,23 +11,40 @@ $b_sliders.each(
         var $wrap = $frame.parent();
 
         // Call Sly on frame
-        var slider_options = {
-            horizontal: 1,
-            itemNav: 'basic',
-            smart: 1,
-            activateOn: 'click',
-            mouseDragging: 1,
-            touchDragging: 1,
-            releaseSwing: 1,
-            startAt: 1,
-            scrollBy: 3,
-            speed: 300,
-            dragHandle: 1,
-            dynamicHandle: 1,
-            clickBar: 1,
-            pagesBar: $b_slider.find('.b-stories__pages'),
-            activatePageOn: 'click',
-        };
+        var slider_options = {};
+        if (jQuery(window).width() > 767) {
+            slider_options = {
+                horizontal: 1,
+                itemNav: 'basic',
+                smart: 1,
+                activateOn: 'click',
+                mouseDragging: 1,
+                touchDragging: 1,
+                releaseSwing: 1,
+                startAt: 1,
+                scrollBy: 3,
+                speed: 300,
+                dragHandle: 1,
+                dynamicHandle: 1,
+                clickBar: 1,
+                pagesBar: $b_slider.find('.b-stories__pages'),
+                activatePageOn: 'click',
+            };
+        } else {
+            slider_options = {
+                mouseDragging: 1,
+                touchDragging: 1,
+                releaseSwing: 1,
+                speed: 300,
+                activatePageOn: 'click',
+                scrollBy: 100,
+                dragHandle: 1,
+                dynamicHandle: 1,
+                clickBar: 1,
+                pagesBar: $b_slider.find('.b-stories__pages'),
+            };
+
+        }
 
         var $slider_object = new Sly($wrap, slider_options).init();
     }
